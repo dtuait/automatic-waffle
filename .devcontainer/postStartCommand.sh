b18143b0e8330771274c7603db8090af23d0d78a
Victor Reipur <70368750+vicre@users.noreply.github.com>
2025-07-29 16:01:26 +0200

    Handle missing dependency files

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
-composer install --no-interaction || true
-npm install || true
+if [ -f composer.json ]; then
+    composer install --no-interaction
+fi
+
+if [ -f package.json ]; then
+    npm install
+fi
 cp .env.example .env 2>/dev/null || true
